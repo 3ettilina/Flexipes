b84c2d7dcde212ea8084a93caeefb088c5b2a46d
Bettina Carrizo <betticarrizo@gmail.com>
2021-12-30 12:04:03 -0300

    Shuffle recipe selector

--- Flexipes/ViewController.swift
+++ Flexipes/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recipeImageView: UIImageView!
+    
+    var pictures = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pictures = [#imageLiteral(resourceName: "pudding_chia_manzana"), #imageLiteral(resourceName: "hamburguesa_porotos"), #imageLiteral(resourceName: "brownie_porotos"), #imageLiteral(resourceName: "panqueque_avena_banana"), #imageLiteral(resourceName: "empanadas_humita"), #imageLiteral(resourceName: "pudding_chia_chocolate"), #imageLiteral(resourceName: "calabacin_capresse")]
+        
+        recipeImageView.image = #imageLiteral(resourceName: "flexipes")
     }
-
-
+    @IBAction func shuffleButtonPressed(_ sender: Any) {
+        recipeImageView.image = getRandomImage()
+        recipeImageView.layer.opacity = 1.0
+    }
+    
+    func getRandomImageIndex(_ size: Int) -> Int {
+        Int.random(in: 0..<size)
+    }
+    
+    func getRandomImage() -> UIImage {
+        pictures[getRandomImageIndex(pictures.count)]
+    }
 }
-
